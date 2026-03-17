15781796dfc6358d55b2ae946b3add1b3dc4bda7
Lisa Kowalczik <lisa.kowalczik123@gmail.com>
2026-03-17 14:56:59 +0100

    neue früchte

--- snake.js
+++ snake.js
@@ -5,7 +5,7 @@ const scoreCanvas = document.getElementById("score");
 const scoreCtx = scoreCanvas.getContext("2d");
 const canvas = document.getElementById("spielfeld")
 const ctx = canvas.getContext("2d")
-//Für handy spiel bar
+//Für handy spielbar
 function resizeCanvas() {
     // quadratisches Spielfeld, maximal 90% der kleineren Bildschirm-Seite
     const size = Math.min(window.innerWidth, window.innerHeight) * 0.9;
@@ -25,12 +25,28 @@ apfelIcon.src = "images/Apfel.png"; // Passe den Pfad ggf. an!
 let apfelIconGeladen = false;
 apfelIcon.onload = () => {
     apfelIconGeladen = true; };
+
 //Bananen Bild
 const bananeIcon = new Image();
 bananeIcon.src = "images/Banane.png"; // Passe den Pfad ggf. an!
 let bananeIconGeladen = false;
 bananeIcon.onload = () => {
     bananeIconGeladen = true; };
+
+//Melonen Bild
+const meloneIcon = new Image();
+meloneIcon.src = "images/Melone.png"; // Passe den Pfad ggf. an!
+let meloneIconGeladen = false;
+meloneIcon.onload = () => {
+    meloneIconGeladen = true; };
+
+//Kokosnuss Bild
+const KokosnussIcon = new Image();
+KokosnussIcon.src = "images/Kokosnuss.png"; // Passe den Pfad ggf. an!
+let KokosnussIconGeladen = false;
+KokosnussIcon.onload = () => {
+    KokosnussIconGeladen = true;};
+
 //Schlangen Bild
 const snakeHeadImg = new Image();
 snakeHeadImg.src = "images/Schlangen-Kopf.png";   // Passe den Pfad ggf. an!
@@ -47,6 +63,7 @@ const gridSize = 20;     // Größe eines Feldes (Pixel)
 const gridCount = 20;    // Anzahl Felder pro Reihe/Spalte
 
 //Variablen festlegen
+let lastTapTime = 0;
 let touchStartX = 0, touchStartY = 0;
 let touchEndX = 0, touchEndY = 0;
 let direction = "right" // start richtung von der schlange
@@ -57,8 +74,13 @@ let score = 0; // score
 //Variablen festlegen
 let appleX, appleY;
 let bananaX, bananaY;
+let meloneX, meloneY;
+let kokosnussX, kokosnussY;
+
 generateApple();
-generateBanane()
+generateBanane();
+generateMelone();
+generateKokosnuss();
 
 // Score-Funktion (zeichnet den Score im blauen Kasten)
 function updateScore() {
@@ -87,12 +109,13 @@ document.addEventListener("keydown", function(event) {
         direction = "up";
     }
 })
-//Touch richtungen
+//Touch funktionen
 canvas.addEventListener('touchstart', function(event) {
     // Nur einen Finger beachten
     if(event.touches.length === 1){
         touchStartX = event.touches[0].clientX;
         touchStartY = event.touches[0].clientY;
+        event.preventDefault(); // Verhindert Scrollen beim Touchstart!
     }
 });
 
@@ -100,6 +123,20 @@ canvas.addEventListener('touchend', function(event) {
     touchEndX = event.changedTouches[0].clientX;
     touchEndY = event.changedTouches[0].clientY;
     handleSwipe();
+
+    // Double Tap
+    const currentTime = Date.now();
+    if (currentTime - lastTapTime < 300 && gameOver) { // Nur wenn gameOver!
+        gameOver = false;
+        resetGame();
+    }
+    lastTapTime = currentTime;
+
+    event.preventDefault(); // Verhindert Scrollen beim Touchend!
+});
+
+canvas.addEventListener('touchmove', function(event) {
+    event.preventDefault(); // Verhindert Scrollen beim Bewegen des Fingers auf dem Canvas!
 });
 
 // Prüfe, ob Feld frei ist (weder Schlange noch andere Frucht)
@@ -135,6 +172,37 @@ function generateBanane() {
     bananaX = x;
     bananaY = y;
 }
+
+// Die Melone generieren
+function generateMelone() {
+    let x, y;
+    do {
+        x = Math.floor(Math.random() * gridCount) * gridSize;
+        y = Math.floor(Math.random() * gridCount) * gridSize;
+    } while (!isFree(x, y, [
+        { x: appleX, y: appleY },
+        { x: bananaX, y: bananaY },
+        { x: kokosnussX, y: kokosnussY }
+    ]));
+    meloneX = x;
+    meloneY = y;
+}
+
+// Die Kokosnuss generieren
+function generateKokosnuss() {
+    let x, y;
+    do {
+        x = Math.floor(Math.random() * gridCount) * gridSize;
+        y = Math.floor(Math.random() * gridCount) * gridSize;
+    } while (!isFree(x, y, [
+        { x: appleX, y: appleY },
+        { x: bananaX, y: bananaY },
+        { x: meloneX, y: meloneY }
+    ]));
+    kokosnussX = x;
+    kokosnussY = y;
+}
+
 //Die Schlange Generieren
     function moveSnake() {
         // neuen Kopf berechnen
@@ -154,6 +222,14 @@ function generateBanane() {
             score ++ //score soll eins höher gehen
             updateScore();
             generateBanane(); // Banane neu generieren, aber nie auf dem apfel
+        } else if (newHead.x === meloneX && newHead.y === meloneY) { // wenn die schlange die banane frisst
+            score ++ //score soll eins höher gehen
+            updateScore();
+            generateMelone(); // Melone neu generieren, aber nie auf dem apfel
+        } else if (newHead.x === kokosnussX && newHead.y === kokosnussY) { // wenn die schlange die banane frisst
+            score ++ //score soll eins höher gehen
+            updateScore();
+            generateKokosnuss(); // Kokosnuss neu generieren, aber nie auf dem apfel
         } else {
             // sonst: letztes Teil entfernen (Länge bleibt gleich)
             snake.pop();
@@ -199,6 +275,8 @@ function handleSwipe() {
         else if (dy < -threshold && direction !== "down") direction = "up";
     }
 }
+timer = null;
+
 // Beim neustart vom game das alles wieder reset
 function resetGame() {
     snake = [{ x: 20, y: 20 }];
@@ -208,6 +286,10 @@ function resetGame() {
     generateApple();
     generateBanane();
     gameOver = false;
+    if (timer) {
+        clearInterval(timer);
+        timer = null;
+    }
     timer = setInterval(gameLoop, 200);
 }
 
@@ -231,7 +313,7 @@ function resetGame() {
             scoreCtx.fillText("Gewonnen! :)", centerX, centerY - 10);
 
             scoreCtx.font = "15px Arial";
-            scoreCtx.fillText("Drücke Enter um neu zu starten!", centerX, centerY + 18);
+            scoreCtx.fillText("Drücke Enter oder tippe zweimal auf den Bildschirm, um neu zu starten!", centerX, centerY + 18);
             return;
         }
 
@@ -256,12 +338,12 @@ function resetGame() {
             scoreCtx.fillStyle = "red";
             scoreCtx.font = "15px Arial";
             scoreCtx.fillText("Game Over!", centerX, centerY - 10);
-            scoreCtx.fillText("Drücke Enter um neu zu starten!", centerX, centerY + 18);
+            scoreCtx.fillText("Drücke Enter oder tippe zweimal auf den Bildschirm, um neu zu starten!", centerX, centerY + 18);
             return; // Funktion abbrechen, damit nichts mehr gezeichnet wird
 
         }
 
-        // Das spiel endet wenn die schlange sich selbst berührt
+        // Das spiel endet wenn, die schlange sich selbst berührt
         for (let i = 1; i < snake.length; i++) {
             if (head.x === snake[i].x && head.y === snake[i].y) {
                 clearInterval(timer);
@@ -278,7 +360,7 @@ function resetGame() {
                 scoreCtx.fillStyle = "red";
                 scoreCtx.font = "15px Arial";
                 scoreCtx.fillText("Game Over!", centerX, centerY - 10);
-                scoreCtx.fillText("Drücke Enter um neu zu starten!", centerX, centerY + 18);
+                scoreCtx.fillText("Drücke Enter oder tippe zweimal auf den Bildschirm, um neu zu starten!", centerX, centerY + 18);
                 return;
             }
         }
@@ -293,6 +375,26 @@ function resetGame() {
             ctx.fillStyle = "red";
             ctx.fillRect(appleX, appleY, gridSize, gridSize);
         }
+        // Die Banane zeichnen
+        if (bananeIconGeladen) {
+            ctx.drawImage(bananeIcon, bananaX, bananaY, gridSize, gridSize);
+        } else {
+            ctx.fillStyle = "red";
+            ctx.fillRect( bananaX, bananaY, gridSize, gridSize);
+        }
+        if (meloneIconGeladen) {
+            ctx.drawImage(meloneIcon, meloneX, meloneY, gridSize, gridSize);
+        } else {
+            ctx.fillStyle = "pink";
+            ctx.fillRect(meloneX, meloneY, gridSize, gridSize);
+        }
+        if (KokosnussIconGeladen) {
+            ctx.drawImage(KokosnussIcon, kokosnussX, kokosnussY, gridSize, gridSize);
+        } else {
+            ctx.fillStyle = "brown";
+            ctx.fillRect(kokosnussX, kokosnussY, gridSize, gridSize);
+        }
+
         for (let i = 0; i < snake.length; i++) {
             if (i === 0 && snakeHeadImgLoaded) {
                 drawRotatedImage(snakeHeadImg, snake[0].x, snake[0].y, gridSize, direction);
@@ -301,22 +403,7 @@ function resetGame() {
                 const segmentDirection = getSegmentDirection(snake[i], snake[i - 1]);
                 drawRotatedImage(snakeBodyImg, snake[i].x, snake[i].y, gridSize, segmentDirection);
             }
-            // Die Banane zeichnen
-            if (bananeIconGeladen) {
-                ctx.drawImage(bananeIcon, bananaX, bananaY, gridSize, gridSize);
-            } else {
-                ctx.fillStyle = "red";
-                ctx.fillRect( bananaX, bananaY, gridSize, gridSize);
-            }
-            for (let i = 0; i < snake.length; i++) {
-                if (i === 0 && snakeHeadImgLoaded) {
-                    drawRotatedImage(snakeHeadImg, snake[0].x, snake[0].y, gridSize, direction);
-                } else if (i > 0 && snakeBodyImgLoaded) {
-                    // Richtung berechnen: vom vorherigen Segment zum aktuellen
-                    const segmentDirection = getSegmentDirection(snake[i], snake[i - 1]);
-                    drawRotatedImage(snakeBodyImg, snake[i].x, snake[i].y, gridSize, segmentDirection);
-                }
-            }
         }
-    }
-let timer = setInterval(gameLoop, 200);
+}
+
+timer = setInterval(gameLoop, 200);
